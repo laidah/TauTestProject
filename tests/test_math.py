e5ccbf05dfed2eeb15560ea48978e9edb3ba66aa
laidah <hinata-tlt@ya.ru>
2022-12-19 10:42:33 +0100

    parametrized test with hypothesis & comments improvement

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,20 +1,26 @@
-import csv
 import pytest
+from hypothesis import given, strategies as st
 
 
+# ----------------
 # Simple math test
+# ----------------
 def test_one_plus_one():
     assert 1 + 1 == 2
 
 
+# --------------------------
 # Test verifies an exception
+# --------------------------
 def test_division_by_zero_exception():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
     assert "division by zero" in str(e.value)
 
 
+# -------------------------------------
 # Parametrized tests for multiplication
+# -------------------------------------
 parameters = [
     (2, 3, 6),          # positives
     (1, 99, 99),        # identity
@@ -29,8 +35,9 @@ parameters = [
 def test_multiplication(a, b, result):
     assert a * b == result
 
-
+# ----------------------------------------
 # Parametrized tests reading data from csv
+# ----------------------------------------
 def get_data(csv_name):
     with open(csv_name) as csv_file:
         params = []
@@ -39,6 +46,25 @@ def get_data(csv_name):
     return params
 
 
-@pytest.mark.parametrize('a, b, result', get_data('../multiply_test_data.csv'))
+@pytest.mark.parametrize('a, b, result', get_data('multiply_test_data.csv'))
 def test_multiplication(a, b, result):
     assert a * b == result
+
+
+# ----------------------------
+# Parametrized with hypothesis
+# ----------------------------
+def multiplication(a, b):
+    return a*b
+
+
+@given(st.integers(), st.integers())
+def test_multiplication(a, b):
+    assert multiplication(a, b) == multiplication(b, a)
+
+
+@given(st.floats(allow_nan=False, allow_subnormal=False, allow_infinity=False), st.floats(allow_nan=False,
+        allow_subnormal=False, allow_infinity=False))
+def test_multiplication(a, b):
+    print("a = {}, b={}".format(a, b))
+    assert multiplication(a, b) == multiplication(b, a)
